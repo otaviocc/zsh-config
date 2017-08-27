7e4c721835b0271004764393b316345256dabef3
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2017-08-27 12:10:29 +0200

    Splits development into several files

--- load/development-xcode.bash
+++ load/development-xcode.bash
@@ -1,4 +1,3 @@
-# xcode shortcut
 function openx {
     workspace=$(find . -maxdepth 1 -name '*.xcworkspace' -print -quit)
     project=$(find . -maxdepth 1 -name '*.xcodeproj' -print -quit)
@@ -11,16 +10,3 @@ function openx {
         echo "No Xcode workspace or project found"
     fi
 }
-
-# mute dev tools
-export COCOAPODS_DISABLE_STATS=1
-
-export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-
-export FASTLANE_OPT_OUT_USAGE=1
-export FASTLANE_SKIP_UPDATE_CHECK=1
-
-# ruby
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
