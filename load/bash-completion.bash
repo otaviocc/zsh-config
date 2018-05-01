f3c2c5cdac4f83d5152d64a23eb6360a156a32a4
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2018-05-01 22:02:09 +0200

    Update colors

--- load/bash-completion.bash
+++ load/bash-completion.bash
@@ -3,6 +3,6 @@ shopt -s cmdhist
 shopt -s dotglob
 shopt -s checkwinsize
 
-bind 'set show-all-if-ambiguous on'
+# bind 'set show-all-if-ambiguous on'
 bind 'set completion-ignore-case on'
-bind 'TAB:menu-complete'
+# bind 'TAB:menu-complete'
