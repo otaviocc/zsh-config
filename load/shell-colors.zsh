1fa3ab61a328a9ccbea147f17aff9aba1f9d246e
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2019-06-26 16:20:39 +0200

    Fix colors used by menu completion

--- load/shell-colors.zsh
+++ load/shell-colors.zsh
@@ -1,4 +1,6 @@
-export CLICOLOR=1
-export LSCOLORS=ExFxCxDxBxEgEdAbAgAcAd
+export CLICOLOR=YES
 
-zstyle ':completion:*:default' list-colors ${(s.:.)LSCOLORS}
+export LSCOLORS="ExFxCxDxBxEgEdAbAgAcAd"
+export LS_COLORS="di=1;34:ln=1;35:so=1;32:pi=1;33:ex=1;31:bd=1;34;46:cd=1;34;43:su=1;30;41:sg=1;30;46:tw=1;30;42:ow=1;30;43"
+
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
