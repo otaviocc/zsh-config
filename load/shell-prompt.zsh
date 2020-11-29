78b9dacb726d3c95a9943289a84036da74c854aa
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2020-11-29 18:14:54 +0100

    Add Git Information

--- load/shell-prompt.zsh
+++ load/shell-prompt.zsh
@@ -1,2 +1,22 @@
-unset PS1 PROMPT
-PS1='%F{244}$ %F{reset}'
+unset PS1 PROMPT RPROMPT
+
+# PS1
+PROMPT='%(?.%F{green}√.%F{red}?%?)%f %B%F{240}%1~%f%b %# '
+
+# RPROMPT
+setopt prompt_subst
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
+zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f'
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+zstyle ':vcs_info:*' enable git cvs svn
+
+vcs_info_wrapper() {
+    vcs_info
+    if [ -n "$vcs_info_msg_0_" ]; then
+        echo "%{$fg[grey]%}${vcs_info_msg_0_}%{$reset_color%}$del"
+    fi
+}
+
+RPROMPT=$'$(vcs_info_wrapper)'
